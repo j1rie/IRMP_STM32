87425a4595aa3237b5b7209a178ae89c92922fed
j1rie <j1rie@users.noreply.github.com>
2018-01-17 14:05:39 +0100

    switch to using lircd-uinput (having fixed lircd-uinput) and eventlircd

---
+++ irmplircd/LibreELEC/uninstall.sh
@@ -0,0 +1,8 @@
+rm -r ~/.kodi/addons/virtual.irmp_stm32/
+rm ~/.config/system.d/irmplircd.service
+rm ~/.config/system.d/lircd-uinput.service
+rm ~/.config/lirc_options.conf
+rm ~/.config/autostart.sh
+rm ~/.config/udev.rules.d/80-irmp.rules
+rm -r ~/.config/system.d/dev-irmp_stm32.device.wants
+rm -r~/.config/system.d/irmplircd.service.wants
