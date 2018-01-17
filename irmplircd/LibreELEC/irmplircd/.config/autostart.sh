30dd71ee86305ed5aa0ef11a3fc32f9f3a64e300
j1rie <j1rie@users.noreply.github.com>
2018-01-17 19:01:17 +0100

    switch from BindsTo to Requisite in order to avoid boot delay if receiver is not present

---
+++ irmplircd/LibreELEC/irmplircd/.config/autostart.sh
@@ -0,0 +1,1 @@
+ir-keytable -D 5000 -P 1000 -d /dev/input/event9
--- irmplircd/LibreELEC/uninstall.sh
+++ irmplircd/LibreELEC/uninstall.sh
@@ -1,8 +1,8 @@
-rm -r ~/.kodi/addons/virtual.irmp_stm32/
-rm ~/.config/system.d/irmplircd.service
-rm ~/.config/system.d/lircd-uinput.service
-rm ~/.config/lirc_options.conf
-rm ~/.config/autostart.sh
-rm ~/.config/udev.rules.d/80-irmp.rules
-rm -r ~/.config/system.d/dev-irmp_stm32.device.wants
-rm -r~/.config/system.d/irmplircd.service.wants
+rm -r -v ~/.kodi/addons/virtual.irmp_stm32/
+rm -v ~/.config/system.d/irmplircd.service
+rm -v ~/.config/system.d/lircd-uinput.service
+rm -v ~/.config/lirc_options.conf
+rm -v ~/.config/autostart.sh
+rm -v ~/.config/udev.rules.d/80-irmp.rules
+rm -r -v ~/.config/system.d/dev-irmp_stm32.device.wants
+rm -r -v ~/.config/system.d/irmplircd.service.wants
