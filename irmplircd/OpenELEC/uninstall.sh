1cec91c7e9ff7e32afcffc930518577233779290
j1rie <j1rie@users.noreply.github.com>
2018-01-18 23:30:09 +0100

    easy installation on OpenELEC

---
+++ irmplircd/OpenELEC/uninstall.sh
@@ -0,0 +1,8 @@
+rm -r -v ~/.kodi/addons/virtual.irmp_stm32/
+rm -v ~/.config/system.d/irmplircd.service
+rm -v ~/.config/system.d/lircd-uinput.service
+rm -v ~/.config/lirc_options.conf
+rm -v ~/.config/autostart.sh
+rm -v ~/.config/udev.rules.d/80-irmp.rules
+rm -r -v ~/.config/system.d/dev-irmp_stm32.device.wants
+rm -r -v ~/.config/system.d/irmplircd.service.wants
