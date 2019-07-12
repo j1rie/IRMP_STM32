c162c945a926a397da97926524f15b43c628bc29
j1rie <j1rie@users.noreply.github.com>
2019-07-12 13:29:13 +0200

    log only once

--- irmplircd/log_KEY_REFRESH.sh
+++ irmplircd/log_KEY_REFRESH.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-#TODO: do this only once
-date >> /var/log/started_by_IRMP_STM32
+if [ "$(stat -c %Y /var/log/started_by_IRMP_STM32)" -lt "$(stat -c %Y /var/log/boot.msg)" ];
+then date >> /var/log/started_by_IRMP_STM32; fi;
