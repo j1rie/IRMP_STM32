ce3d4b394fa6c44cdbb1509ce6533f80fdc16619
j1rie <j1rie@users.noreply.github.com>
2020-05-20 22:05:56 +0200

    loop longer in Write_and_Check(), silence warning, zstd needed for MSYS2 in Makefile, remove-O0 and -MD from Makefile, PATHSEP already defined, disconnect before rescan, show non-.bin files fully, Ctors and Dtor not needed, indentation

--- stm32IRconfig_gui/Linux/upgrade.h
+++ stm32IRconfig_gui/Linux/upgrade.h
@@ -10,14 +10,11 @@ private:
 	char* printcollect{NULL};
 	FXGUISignal* guisignal{NULL};
 public:
-	Upgrade();
-	Upgrade(const char* pfirmwarefile, char* pprint, char* pprintcollect, FXGUISignal* pguisignal);
 	void set_firmwarefile(const char* pfirmwarefile);
 	void set_print(char* pprint);
 	void set_printcollect(char* pprintcollect);
 	void set_signal(FXGUISignal* pguisignal);
 	FXint run();
-	~Upgrade();
 };
 
 #endif
