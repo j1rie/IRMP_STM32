557a051b6e9c83c43e8d8b0b143de21f97a572aa
j1rie <j1rie@users.noreply.github.com>
2020-07-18 15:16:02 +0200

    gui: clear debug messages also if device doesn‘t reappear

--- stm32IRconfig_gui/Linux/stm32IRconfig_gui.cpp
+++ stm32IRconfig_gui/Linux/stm32IRconfig_gui.cpp
@@ -1735,7 +1735,8 @@ MainWindow::onPrint(FXObject *sender, FXSelector sel, void *ptr)
 				device_list->deselectItem(0);
 				device_list->selectItem(cur_item);
 				onConnect(NULL, 0, NULL);
-			}
+			} else
+				input_text->setText("");
 			FXString u = printcollect;
 			input_text->appendText(u);
 			input_text->setBottomLine(INT_MAX);
