ea739b8cbb598a1ad1d2c8df516a9e4600b065f4
j1rie <j1rie@users.noreply.github.com>
2024-01-18 23:38:49 +0100

    change statusled pin for RP2040-One

--- RP2040/src/config.h
+++ RP2040/src/config.h
@@ -29,6 +29,6 @@
 #define IR_OUT_GPIO		6  /* GPIO 6  (Pin 9)  */
 #define WAKEUP_RESET_GPIO	7  /* GPIO 7  (Pin 10) */
 #define LED_GPIO		8  /* GPIO 8  (Pin 11) */
-#define STATUSLED_GPIO		22 /* GPIO 22 (Pin 29) */
+#define STATUSLED_GPIO		2  /* GPIO 2  (Pin 4)  */
 
 #endif /* __CONFIG_H */
