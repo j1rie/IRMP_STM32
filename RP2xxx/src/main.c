e8fe200700012db7644a8836b61e2560b7465acb
j1rie <j1rie@users.noreply.github.com>
2025-04-25 11:08:09 +0200

    adapt tusb_init() to new style

--- RP2xxx/src/main.c
+++ RP2xxx/src/main.c
@@ -751,7 +751,8 @@ int main(void)
 	board_init();
 	LED_Switch_init();
 	Systick_Init();
-	tusb_init();
+	tusb_rhport_init_t dev_init = { .role = TUSB_ROLE_DEVICE, .speed = TUSB_SPEED_AUTO };
+	tusb_init(BOARD_TUD_RHPORT, &dev_init);
 	IRMP_Init();
 	irsnd_init();
 	ws2812_init();
